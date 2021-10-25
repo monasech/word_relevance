653e668b11208cc065c150a5a4674dea377b5fca
monasech <17804515+monasech@users.noreply.github.com>
2021-10-25 08:15:48 +0300

    finished the unique word finder and repeated word counter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import time
+
 from pandas.core.frame import DataFrame
 #from filename_parser import filenameParser
 from unique_word_finder import unique_word_finder
 from repeated_word_finder import repeated_word_finder
 import pandas as pd
-path = '/Users/momo/src/training/word_relevance/data'
+path = '/home/mohammed/src/personal_learning/work_relevance/data/'
 column_names = ['words']
 
 
@@ -11,6 +13,7 @@ column_names = ['words']
 
 
 
+start_time = time.time()
 
 # Begin by creating an empty dataframe, then populate it with the unique word column
 df_init = pd.DataFrame()
@@ -20,11 +23,21 @@ df_init['words'] = unique_word_finder(path)
 #Logs the success of the unique word finder function
 print(df_init.info())
 
+df_counted = pd.DataFrame()
+
 df_counted = repeated_word_finder(df_init,path)
 
 print(df_counted.info())
 
+df_counted.to_csv(r'/home/mohammed/src/personal_learning/work_relevance/export/dftocsv.csv', sep='\t', encoding='utf-8', header='true')
+
+print("--- %s seconds ---" % (time.time() - start_time))
+# Note, the current implementation takes roughly 120 Seconds or so to run
+# TODO: Create a log file for the program that details the time taken for each step
+
 '''
+df.to_csv(unique_words_counted.csv)
+
 csv_file = open(path + '/unique_words.csv','w')
 
 writer = csv.writer(csv_file)
--- src/repeated_word_finder.py
+++ src/repeated_word_finder.py
@@ -8,23 +8,29 @@ import pandas as pd
 
 
 # Then need to create a dataframe with the all the words in the words column, and all file names in the rest of the columns, with values set to zero initially.
-def repeated_word_finder(data,path):
+def repeated_word_finder(df,path):
     '''This function will parse the dataframe for the selected word, and increment the counter for its appearance in the document it was found in'''
     filenames = filenameParser(path)
     for f in filenames:
+        # TODO: Need to insert a column with the new filename into the dataframe
         print(f)
         appended_filename = str(f.replace('.txt',''))
-        print(appended_filename)
-        data[appended_filename] = 0
-        print(data.info())
+        print("The Documents name is: " + appended_filename )
+        df[appended_filename] = 0
+        print(df.info())
         print("opening the document")
         open_doc = open(path + '/' + f,'r')
         print("document opened")
         for line in open_doc:
             print("canary first for loop")
             for word in line.split():
-                print("canary second for loop")
-                dfrow_index = data.index[data['words'] == word]
-                print(dfrow_index)
-                data.at[dfrow_index,appended_filename] += 1
-        
+                # TODO: Check for None or Empty
+                if word:
+                    print("canary second for loop")
+                    #dfrow_index = df.index[df['words'].value == word]
+                    #print(dfrow_index)
+                    word_index = (df[df['words']==word].index.values)
+                    print(word_index)
+                    # Have managed to pull the row index for the word, TODO: need to figure out the counter incrementation
+                    df.ix[word_index,appended_filename] += 1
+    return df
