84c87d67f12b6da34672966ac8d3f0a45d84fffe
malnassar <17804515+monasech@users.noreply.github.com>
2021-10-27 19:52:03 +0300

    Relevance calculator is working, need to debug the word importance and word relevance functions. However, columns and NaN values are being created within the dataframe for each document.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from os import name
 import time
-from pandas.core.frame import DataFrame
+#from pandas.core.frame import DataFrame
 from relevancy_calculator import query_breakup, word_importance, word_locator, generality_discount, word_relevance
 from word_finder import filenameParser,unique_word_finder,repeated_word_finder
 import pandas as pd
 import numpy as np
-from tkinter.filedialog import askdirectory, askopenfilename
+#from tkinter.filedialog import askdirectory, askopenfilename
 
 verby = input("run from phase 1? (y/n)")
 
@@ -14,7 +14,10 @@ if verby == "y":
 
     print("Running Phase 1")
     #path = '/home/mohammed/src/personal_learning/work_relevance/data/'
-    path = askdirectory()
+    data_path = '/Users/momo/src/training/word_relevance/data/'
+    export_path_csv_1 = '/Users/momo/src/training/word_relevance/export/dft_init_to_csv.csv'
+    export_path_csv_2 = '/Users/momo/src/training/word_relevance/export/dft_counted_to_csv.csv'
+    export_path_csv_3 = '/Users/momo/src/training/word_relevance/export/dft_result_to_csv.csv'
     column_names = ['words']
 
     start_time = time.time()
@@ -25,25 +28,25 @@ if verby == "y":
 
     df_init = pd.DataFrame()
 
-    df_init['words'] = unique_word_finder(path) 
+    df_init['words'] = unique_word_finder(data_path) 
     print("--- %s seconds ---" % (time.time() - start_time))
 
     #Logs the success of the unique word finder function, and save the initial dataframe to a csv
     print(df_init)
-    csv_init_directory = askopenfilename()
-    df_init.to_csv(csv_init_directory, encoding='utf-8', header=True, index=False)
+    #csv_init_directory = askopenfilename()
+    df_init.to_csv(export_path_csv_1, encoding='utf-8', header=True, index=False)
 
     # Create a new dataframe to hold the counters for each word.
     df_counted = pd.DataFrame()
 
-    df_counted = repeated_word_finder(df_init,path)
+    df_counted = repeated_word_finder(df_init,data_path)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
 
     print(df_counted)
 
-    df_counted.to_csv('/home/mohammed/src/personal_learning/work_relevance/export/dft_counted_to_csv.csv',index=False)
+    df_counted.to_csv(export_path_csv_2,index=False)
 
     print("--- %s seconds ---" % (time.time() - start_time))
 
@@ -56,11 +59,15 @@ print("Begin phase two")
 input_string = "tennis match"
 
 try: df_pass
-except NameError: df_pass = pd.read_csv('/home/mohammed/src/personal_learning/work_relevance/export/dft_counted_to_csv.csv')
+except NameError: df_pass = pd.read_csv('/Users/momo/src/training/word_relevance/export/dft_counted_to_csv.csv')
 else: pass
 
-try: path
-except NameError: path = '/home/mohammed/src/personal_learning/work_relevance/data'
+try: data_path
+except NameError: data_path = '/Users/momo/src/training/word_relevance/data/'
+else: pass
+
+try: export_path_csv_3
+except NameError: export_path_csv_3 = '/Users/momo/src/training/word_relevance/export/dft_result_to_csv.csv'
 else: pass
 
 word_list = query_breakup(input_string)
@@ -72,12 +79,17 @@ df_result = pd.DataFrame()
 
 df_result["word_query"] = word_list
 
-names_of_documents = filenameParser(path)
+names_of_documents = filenameParser(data_path)
 
 for word in word_list:
     
-    word_index = (df_result[df_result["word_query"] == word].index.values)
-    word_row = np.array(df_result.iloc[word_index,2:])
+    word_index_result = (df_result[df_result["word_query"] == word].index.values)
+    
+    word_index_pass = (df_pass[df_pass["words"] == word].index.values)
+    print("Word index incoming")
+    print(word_index_result)
+    print(word_index_pass)
+    word_row = np.array(df_result.iloc[word_index_result,2:])
     
     number_of_documents_with_word = len(word_locator(df_pass,word,'words'))
     #print(df_pass)
@@ -87,23 +99,31 @@ for word in word_list:
     #df_result["generality discount"] = 0
 
     for name in names_of_documents:
+        appended_name = str(name.replace('.txt',''))
+        print(appended_name)
         if number_of_documents_with_word == 0:
     
             gen_discount = 0
-            df_result.loc[word_index,"generality_discount"] = gen_discount
+            df_result.loc[word_index_result,"generality_discount"] = gen_discount
             print(df_result)
         else: 
             gen_discount = generality_discount(number_of_documents_with_word,number_of_documents)
-            df_result.loc[word_index,"generality_discount"] = gen_discount
+            df_result.loc[word_index_result,"generality_discount"] = gen_discount
             print(df_result)
-
-        df_result.loc[word_index,"word importance of"] = word_importance(df_result.loc(word_index,name).value(),df_result.loc[:,name].sum()) 
-        df_result.loc[word_index,"word relevance of"] = word_relevance(df_result.loc[word_index,"word importance of"].value(),df_result.loc[word_index,"generality_discount"].value()) 
+        word_importance_column_name = "word importance of -> " + appended_name
+        word_relevance_column_name = "word relevance of -> " + appended_name
+        #print(df_result.loc[word_index,appended_name])
+        #print(df_result.loc[:,appended_name].sum())
+        word_importance_value = word_importance(df_pass.loc[word_index_pass,appended_name],df_pass.loc[:,appended_name].sum()) 
+        df_result.loc[word_index_result,word_importance_column_name] = word_importance_value
+        word_relevance_value = word_relevance(df_result.loc[word_index_result,word_importance_column_name],df_result.loc[word_index_result,"generality_discount"])
+        
+        df_result.loc[word_index_result,word_relevance_column_name] =  word_relevance_value
 
     
 
     print(df_result)
-
+df_result.to_csv(export_path_csv_3,index=False)
 
 
 
